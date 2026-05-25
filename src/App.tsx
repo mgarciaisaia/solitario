bf70934264d8d99b584650916ba581d8689f0bb4
mgarciaisaia <mgarciaisaia+github@gmail.com>
2026-05-25 02:30:24 -0300

    Scaffold tech stack

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="min-h-screen bg-green-800 text-white flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">Solitaire</h1>
+    </main>
+  );
+}
